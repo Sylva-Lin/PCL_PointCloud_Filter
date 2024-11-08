00cc9b184591ed0699d55efd7a8d66ca677f4266
Sylva-Lin <lzc130784@163.com>
2024-11-08 15:23:10 +0800

    first

---
+++ radius_filtering.cpp
@@ -0,0 +1,37 @@
+#include <pcl/point_types.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/io/pcd_io.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+    if (argc != 3)
+    {
+        cout << "Usage: " << argv[0] << " <input.pcd> <output.pcd>" << endl;
+        return -1;
+    }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>());
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) == -1){
+        PCL_ERROR("Could not read input file \n");
+        return -1;
+    }
+
+    cout << "Loaded" << cloud->points.size() << "points from" << argv[1] << endl;
+
+    pcl::RadiusOutlierRemoval<pcl::PointXYZ> radius_filter;
+    radius_filter.setInputCloud(cloud);
+    radius_filter.setRadiusSearch(0.8);
+    radius_filter.setMinNeighborsInRadius(2);
+    radius_filter.filter(*cloud_filtered);
+
+    cout << "Filtered cloud contains" << cloud_filtered->points.size() << "points." << endl;
+
+    pcl::io::savePCDFileASCII(argv[2], *cloud_filtered);
+    cout << "Saved filtered point cloud to" << argv[2] << endl;
+
+    return 0;
+}
